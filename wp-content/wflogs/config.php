e472aa7aaebf65f07ca384e3a8bad68e3866bfbc
DESKTOP-BINQO85\user <dhikydhermawan77@gmail.com>
2024-06-10 14:07:22 +0700

    first commit

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:7:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1718176664;s:7:"authKey";s:64:"Vw~YDEv,r~,*Zj%MHAI0L3:!-@,]:~+c+0V;No50[4TG-#UHJ-ch^dPTH8emPCys";s:7:"version";s:5:"1.1.0";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:1064;}
